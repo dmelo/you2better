e36989225ba76444a0433f90a59efdb892e94762
Diogo Oliveira de Melo <dmelo87@gmail.com>
2011-08-21 19:54:41 -0300

    adding youtube-dl as a git submodule

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
 header('Content-Type: audio/mpeg');
-system("./youtube-dl --no-part -q --output=/dev/stdout http://www.youtube.com/watch?v={$youtubeId} | ffmpeg -i - -f mp3 pipe:1 | cat");
+system("./youtube-dl/youtube-dl --no-part -q --output=/dev/stdout http://www.youtube.com/watch?v={$youtubeId} | ffmpeg -i - -f mp3 pipe:1 | cat");
