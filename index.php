d37d30c2728219fc064388d7185451646a9887d4
Diogo Oliveira de Melo <dmelo87@gmail.com>
2011-08-22 00:40:24 -0300

    all fixed

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 
 $youtubeId = $_GET['youtubeid'];
 header("Content-Type: audio/mpeg\n");
-system("./api/youtube-dl/youtube-dl --no-part -q --output=/dev/stdout http://www.youtube.com/watch?v={$youtubeId} | ffmpeg -i - -f mp3 pipe:1 | cat");
-//var_dump($_GET);
+
+// Here is where all the magic happens.
+system("./youtube-dl/youtube-dl --no-part -q --output=/dev/stdout http://www.youtube.com/watch?v={$youtubeId} | ffmpeg -i - -f mp3 pipe:1 | cat");
