5e65b43a56a6c2c91e9b243dc0704843488694f6
Diogo Oliveira de Melo <dmelo87@gmail.com>
2011-09-18 13:08:37 -0300

    fix problem with cache

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 include_once 'conf.php';
 
+$youtubeId = $_GET['youtubeid'];
+
 function writeTimestamp($filename)
 {
     $fd = fopen($filename, 'w');
@@ -9,19 +11,49 @@ function writeTimestamp($filename)
     fclose($fd);
 }
 
-$youtubeId = $_GET['youtubeid'];
+function logFile($str)
+{
+    $fd = fopen(DIRECTORY . '/log-' . $youtubeId, 'a');
+    fwrite($fd, $str. PHP_EOL);
+    fclose($fd);
+}
+
+logFile('start');
 header("Content-Type: audio/mpeg\n");
 
 $filename = DIRECTORY . '/' . $youtubeId . '.mp3';
+$filelock = $filename . '-lock';
 $metaFilename = DIRECTORY . '/internal--' . $youtubeId . '.mp3';
 
+
 writeTimestamp($metaFilename);
-if (is_file($filename)) {
-    header('Content-Length: ' . filesize($filename));
-    echo file_get_contents($filename);
+if (file_exists($filename)) {
+    if (!file_exists($filelock)) {
+        logFile('here 3');
+        header('Content-Length: ' . filesize($filename));
+        echo file_get_contents($filename);
+    } else {
+        logFile('here 2');
+        $offset = 0;
+        $last = 0;
+        while(0 === $last) {
+            clearstatcache();
+            if (!file_exists($filelock))
+                $last = 1;
+            $fd = fopen($filename, 'rb');
+            $size = filesize($filename);
+            fseek($fd, $offset, SEEK_SET);
+            logFile("filename: ${filename}. last: ${last}. offset: ${offset}. size: ${size}" );
+            echo fread($fd, $size - $offset);
+            $offset = $size;
+            fclose($fd);
+            sleep(1);
+        }
+    }
 } else {
+    logFile('here 1');
     // Here is where all the magic happens.
     $ydl = './youtube-dl/youtube-dl --no-part -q';
     $ysite = 'http://www.youtube.com/watch';
-    system("${ydl} --output=/dev/stdout \"${ysite}?v={$youtubeId}\" | ffmpeg -i - -f mp3 pipe:1 | tee ${filename}");
+    system("touch ${filelock}; ${ydl} --output=/dev/stdout \"${ysite}?v={$youtubeId}\" | ffmpeg -i - -f mp3 pipe:1 | tee ${filename}; rm ${filelock}");
 }
