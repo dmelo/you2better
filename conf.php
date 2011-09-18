5db10586255002af12b4777a7fe8cbed65c53ed5
Diogo Oliveira de Melo <dmelo87@gmail.com>
2011-09-18 10:17:23 -0300

    cache

---
+++ conf.php
@@ -0,0 +1,4 @@
+<?php
+
+define('DIRECTORY', 'tmp/cache/');
+define('SIZE_LIMIT', 3048); // Cache size limit in KB.
--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 
+include_once 'conf.php';
+
+function writeTimestamp($filename)
+{
+    $fd = fopen($filename, 'w');
+    fprintf($fd, "%d", time());
+    fclose($fd);
+}
+
 $youtubeId = $_GET['youtubeid'];
 header("Content-Type: audio/mpeg\n");
 
-// Here is where all the magic happens.
-system("./youtube-dl/youtube-dl --no-part -q --output=/dev/stdout http://www.youtube.com/watch?v={$youtubeId} | ffmpeg -i - -f mp3 pipe:1 | cat");
+$filename = DIRECTORY . '/' . $youtubeId . '.mp3';
+$metaFilename = DIRECTORY . '/internal--' . $youtubeId . '.mp3';
+
+writeTimestamp($metaFilename);
+if (is_file($filename)) {
+    header('Content-Length: ' . filesize($filename));
+    echo file_get_contents($filename);
+} else {
+    // Here is where all the magic happens.
+    $ydl = './youtube-dl/youtube-dl --no-part -q';
+    $ysite = 'http://www.youtube.com/watch';
+    system("${ydl} --output=/dev/stdout \"${ysite}?v={$youtubeId}\" | ffmpeg -i - -f mp3 pipe:1 | tee ${filename}");
+}
