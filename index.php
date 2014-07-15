1b6d22508ec6f3323870cbe8bde46975d801252a
Diogo Oliveira de Melo <dmelo87@gmail.com>
2014-07-14 22:57:54 -0300

    download from server

--- index.php
+++ index.php
@@ -16,10 +16,55 @@ if ('mp3' === $ext) {
 
 $ysite = 'http://www.youtube.com/watch';
 $ydl = $conf['ydl'];
-exec("$ydl -g \"{$ysite}?v={$youtubeId}\"", $output, $ret);
+$cacheFilename = realpath(__DIR__ . '/cache/' . $youtubeId);
+$cacheFilenameHeader = "$cacheFilename.$ext.header";
+$cacheFilenameContent = "$cacheFilename.$ext.content";
 
-if (0 != $ret) {
-    header("HTTP/1.0 404 Not Found");
+$isHeader = true;
+if (file_exists($cacheFilenameHeader) && file_exists($cacheFilenameContent)) {
+    $fd = fopen($cacheFilenameHeader, 'r');
+    while (!feof($fd)) {
+        header(fgets($fd, 4096));
+    }
+
+    fclose($fd);
+    $fd = fopen($cacheFilenameContent, 'r');
+    while (!feof($fd)) {
+        echo fgets($fd, 4096);
+    }
 } else {
-    header('Location: ' . $output[0]);
+    exec("$ydl -g \"{$ysite}?v={$youtubeId}\"", $output, $ret);
+    if (0 != $ret) {
+        header("HTTP/1.0 404 Not Found");
+    } else {
+        $url = parse_url($output[0]);
+        $host = 'ssl://' . $url['host'];
+        $uri = $url['path'] . '?' . $url['query'];
+
+        $fp = fsockopen($host, 443);
+        if ($fp) {
+            stream_set_timeout($fp, 10);
+            fwrite($fp, "GET $uri HTTP/1.1" . PHP_EOL . PHP_EOL);
+            
+            $isHeader = true;
+            $fd = fopen($cacheFilenameHeader, 'w');
+            while(!feof($fp)) {
+                $str = fgets($fp, 4096);
+                if ($isHeader && "\r\n" == $str) {
+                    $isHeader = false;
+                    fclose($fd);
+                    $fd = fopen($cacheFilenameContent, 'w');
+                } else {
+                    if ($isHeader) {
+                        header($str);
+                    } else {
+                        echo $str;
+                    }
+                    fwrite($fd, $str);
+                }
+            }
+            fclose($fd);
+            fclose($fp);
+        }
+    }
 }
